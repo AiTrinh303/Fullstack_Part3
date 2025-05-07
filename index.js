a486385a19a3a617c4b5fe449ae9d6f7aaf3f9ef
Ai Trinh <trinhnguyentrongai@gmail.com>
2025-05-07 16:44:40 +0200

    Add frontend production build

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(express.static(path.join(__dirname, 'dist')))
+
+// Fallback for SPA routing
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+})
 
 
 let persons = [
